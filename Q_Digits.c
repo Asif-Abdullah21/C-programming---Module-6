71b4c363487fcda082cca3bb7a4adf20b6740686
Asif-Abdullah21 <asifabdullah214365@gmail.com>
2023-04-10 01:04:25 +0600

    digits

--- Q_Digits.c
+++ Q_Digits.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 int main()
 {
+    int t;
+    scanf("%d", &t);
+
+    for (int i=1;i<=t;i++)
+    {
+        int n;
+        scanf("%d", &n);
+
+        do{
+            printf("%d ",n%10);
+            n = n/10;
+        }while (n!=0);
+
+        printf("\n"); 
+   
+    }
+
+    return 0;
+}
+
+/*
+#include <stdio.h>
+int main()
+{
     int n,rem;
     scanf("%d", &n);
 
@@ -29,3 +53,4 @@ int main()
 
     return 0;
 }
+*/
