4ded507ae890e14ee484229b1f723aae0a8a94f7
Asif-Abdullah21 <asifabdullah214365@gmail.com>
2023-04-09 23:17:07 +0600

    even odd pos neg

---
+++ C_Even_Odd_Positive_and_Negative.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int n,even=0,odd=0,pos=0,neg=0;
+    scanf("%d",&n);
+
+    for(int i=1;i<=n;i++)
+    {
+        int val;
+        scanf("%d",&val);
+
+        if(val%2==0) even++;
+        if(val%2!=0) odd++;
+        if(val>0) pos++;
+        if(val<0) neg++;
+    }
+
+    printf("Even: %d\n",even);
+    printf("Odd: %d\n",odd);
+    printf("Positive: %d\n",pos);
+    printf("Negative: %d\n",neg);
+    
+    return 0;
+}
